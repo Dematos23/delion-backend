54bf734983be4b38ce01fb6b6c12fbab089bec49
Diego Matos <47648806+Dematos23@users.noreply.github.com>
2022-03-06 11:19:36 -0500

    heroku

--- src/services/heroku.service.js
+++ src/services/heroku.service.js
@@ -9,10 +9,10 @@ const { PrismaClient } = Prisma;
 const prisma = new PrismaClient();
 
 export async function main() {
-  await equiposSeed(prisma);
-  //   await adminSeed(prisma);
-  //   await usuariosSeed(prisma);
-  //   await tareasSeed(prisma);
+  //   await equiposSeed(prisma);
+  await adminSeed(prisma);
+  await usuariosSeed(prisma);
+  await tareasSeed(prisma);
   return { message: "Heroku Seed completo" };
 }
 
